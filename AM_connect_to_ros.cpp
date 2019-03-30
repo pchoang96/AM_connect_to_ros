3ea7258a40360daa53af018a7b4a9a469de577e3
pchoang96 <45277369+pchoang96@users.noreply.github.com>
2019-03-30 10:09:03 +0700

    Feed back via rosserial publisher

--- AM_connect_to_ros.cpp
+++ AM_connect_to_ros.cpp
@@ -2,13 +2,18 @@
 #include <std_msgs/String.h>
 #include <std_msgs/Empty.h>
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose2D.h> //x,y,theta float 64
+
 
 #define clkw        0
 #define c_clkw      1
+#define encodPinA1  3
 #define M1_p        6
 #define M1_l        7
+#define encodPinA2  2
 #define M2_p        5
 #define M2_l        4
+#define DEBUG       1
 
 /**----------------pid position calculator------------------------------------------**/
 /* 
@@ -33,21 +38,28 @@ double const r_kP = 0.33, r_kI=3.53,r_kD = 0.004;
 /**--------------------------car parameter-----------------------------------------------**/
 const double pi=3.1415;
 const double sampletime = 0.02, inv_sampletime = 1/sampletime;
-const double wheels_distance = 200, wheels_radius = 32.5, wheels_diameter=65,wheels_encoder = 160;// mm
+const double wheels_distance = 200, wheels_radius = 32.5, wheels_diameter=65,wheels_encoder = 200;// mm
 const double wheel_ticLength = wheels_diameter*pi/wheels_encoder;//0.23mm
 const bool l_motor=1,r_motor=0;
 /*--------------------------position calculation----------------------------*/
 double p_org[]={0.0,0.0,0.0}, p_now[]= {0.0,0.0,0.0}; //{x,y,phi}
-
+int l_p=0,r_p=0;
+double l_d=0,r_d=0;
+//time period for position calulate
+int pos_sampleTime=50;
+long int setting_millis=millis()+pos_sampleTime;
 ros::NodeHandle  nh;
-
+/*----------------------Subscriber define------------------------------------------------*/
 void messageCb(const geometry_msgs::Twist& vel)
 {
-  lin_vel=vel.linear.x;
-  ang_vel=vel.angular.z;
+  lin_vel=vel.linear.x*100;
+  ang_vel=vel.angular.z*180/pi;
 }
 
 ros::Subscriber<geometry_msgs::Twist> sub("/turtle1/cmd_vel", messageCb );
+/*----------------------Publisher define------------------------------------------------*/
+geometry_msgs::Pose2D postef;
+ros::Publisher pos_temp("position", &postef);
 /*
   rosrun rosserial_python serial_node.py /dev/ttyACM0 (aka /dev/tty<-arduino port->)
  * using rostopic to manage and checking topic
@@ -57,6 +69,7 @@ void setup()
 {
   nh.initNode();
   nh.subscribe(sub);
+  nh.advertise(pos_temp);
   pinMode(M1_l,OUTPUT);
   pinMode(M2_l,OUTPUT);
   pinMode(encodPinA1, INPUT_PULLUP);                  // encoder input pin
@@ -75,8 +88,15 @@ void setup()
 
 void loop()
 {
+  if (millis()>=setting_millis)
+    {
+      calculate_position(p_now[0],p_now[1],p_now[2]);
+      postef.x     = p_now[0];
+      postef.y     = p_now[1];
+      postef.theta = p_now[2];
+      setting_millis=millis() + pos_sampleTime;
+    }
   nh.spinOnce();
-  delay(1);
 }
 /*-------------------encoder interrupt 1 ---------------------------------------*/
 void encoder_1()
@@ -159,17 +179,36 @@ void motion(double lin, double phi )
   if (r_set>30) r_set=30;
   else if (r_set<5 && r_set>0.5) r_set =5;
 }
-/*------------------------------------------------------------*/
+/**---------------------------------------------------------------------------------------------------------------**/
+//PID_cal(l_error,l_pre_error,l_integral,l_derivative,l_Ppart,l_Ipart,l_Dpart,l_kP,l_kI,l_kD);
+//PID_cal(r_error,r_pre_error,r_integral,r_derivative,r_Ppart,r_Ipart,r_Dpart,r_kP,r_kI,r_kD);
+//PID_cal(ang_error,ang_pre_error,ang_integral,ang_derivative,ang_Ppart,ang_Ipart,ang_Dpart,p_kP,p_kI,p_kD);
+//PID_cal(lin_error,lin_pre_error,lin_integral,lin_derivative,lin_Ppart,lin_Ipart,lin_Dpart,p_kP,p_kI,p_kD);
+double PID_cal(double error,double pre_error,double _integral,double _derivative,double Ppart,double Ipart,double Dpart,double Kp,double Ki, double Kd)
+{
+  double PID_output;
+    Ppart = Kp*error;
+
+    _integral += error * sampletime;
+    Ipart = Ki*_integral;
+
+    _derivative  = (error  - pre_error )*inv_sampletime;
+    Dpart  = Kd *_derivative ;
+
+    PID_output = Ppart  + Ipart  + Dpart  ;
+    pre_error  = error ;
+    
+    return PID_output;
+}
+/**-------------------------------------------------------------**/
 ISR(TIMER1_OVF_vect) 
 {
-  calculate_position(p_now[0],p_now[1],p_now[2]);
   motion(lin_vel,ang_vel);  
   l_error= l_set-abs(l_p);
   r_error= r_set-abs(r_p);
   if (l_error>=-1 && l_error<=1) l_error=0;
   if (r_error>=-1 && r_error<=1) r_error=0;
   l_out += PID_cal(l_error,l_pre_error,l_integral,l_derivative,l_Ppart,l_Ipart,l_Dpart,l_kP,l_kI,l_kD);
-  r_out = l_out + 5;
   r_out += PID_cal(r_error,r_pre_error,r_integral,r_derivative,r_Ppart,r_Ipart,r_Dpart,r_kP,r_kI,r_kD);
   if (l_out>= 255) l_out = 255;
   if (r_out>= 255) r_out = 255;
